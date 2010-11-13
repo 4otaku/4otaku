70802cb8386945ecbd49ce4589a3fb35383a441b
derinov <derinov@derinov.(none)>
2010-11-13 23:49:50 +0300

    Заливка реплеев для турнира

---
+++ jss/add/replay.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){  	
+
+	window.processing_art = 0;
+	
+	replay = new AjaxUpload('replay_upload', {
+		action: '/engine/upload/postfile.php',
+		name: 'filedata',
+		multiple: true,
+		data: {	  },
+		autoSubmit: true,
+		responseType: 'text/html',
+		onSubmit: function(file, extension) {
+			$(".processing").show();
+			$('#error').html('');
+			window.processing_replay = 1;
+		},
+		onComplete: function(file, response) {
+			window.processing_replay = 0;
+			$(".processing").hide(); 
+			if (response == 'error-maxsize') {
+				$('#error').html('<b>Ошибка! Выбранный вами файл превышает 10 мегабайт.</b>');
+			} else {
+				$('#transparent td').html(response);
+			}
+		}
+	});
+	
+}); 
