98215ab9f2746d4e6b6c71391da54b44c5cc3884
Nameless <noname-nya@yandex.ru>
2010-11-18 08:54:20 +0300

    Фикс автозапуска крона

--- inc.common.php
+++ inc.common.php
@@ -17,9 +17,10 @@ define('SL', DIRECTORY_SEPARATOR);
 define('ROOT_DIR', dirname(__FILE__));
 
 function __autoload($class_name) {
-	$class = 'libs'.SL.str_replace('__',SL,$class_name) . '.php';
-	if (file_exists($class)) include_once $class;
-	else {
+	$class = ROOT_DIR . SL . 'libs'.SL.str_replace('__',SL,$class_name) . '.php';
+	if (file_exists($class)) {
+		include_once $class;
+	} else {
 		if (_INDEX_) {
 			include_once 'templates'.SL.'404'.SL.'fatal.php';
 			ob_end_flush();
