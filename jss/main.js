10b95c20cecfbc173cd16f1186ae8ce92e2584c9
derinov <derinov@derinov.(none)>
2010-12-02 20:27:45 +0300

    Some cookie magic

--- jss/main.js
+++ jss/main.js
@@ -1,3 +1,41 @@
+jQuery.cookie = function (key, value, options) {
+	if (arguments.length > 1 && (value === null || typeof value !== "object")) {
+		options = jQuery.extend({}, options);
+		if (value === null) { options.expires = -1; }
+
+		if (typeof options.expires === 'number') {
+			var days = options.expires, t = options.expires = new Date();
+			t.setDate(t.getDate() + days);
+		}
+
+		return (document.cookie = [
+			encodeURIComponent(key), '=',
+			options.raw ? String(value) : encodeURIComponent(String(value)),
+			options.expires ? '; expires=' + options.expires.toUTCString() : '', 
+			options.path ? '; path=' + options.path : '',
+			options.domain ? '; domain=' + options.domain : '',
+			options.secure ? '; secure' : ''
+		].join(''));
+	}
+
+	options = value || {};
+	var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;
+	return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;
+};
+
+	/* Some Cookie Magic */	
+	
+	if (typeof localStorage == 'object') {
+		var cookie = $.cookie("settings");
+		var local = localStorage.getItem('4otaku_settings');
+
+		if (cookie != null && local == null) {
+			localStorage.setItem('4otaku_settings', cookie);
+		} else if (cookie != local) {
+			$.cookie("settings", local, { expires: 60 , path : window.config.site_dir + '/' });
+		}
+	}
+
 (function($) {
 	$.fn.easyTooltip = function(options){	  
 		var defaults = {	
