cdec165db0d527416decbbeb524295b696703c73
derinov <derinov@derinov.(none)>
2010-12-06 22:11:55 +0300

    Comments output

--- templates/main/comments.php
+++ templates/main/comments.php
@@ -17,7 +17,7 @@
 						if ($sets['user']['rights']) {
 							?>
 								<div class="right">
-									<?=($sets['user']['rights'] > 1 ? $comment['ip'].'&nbsp;' : '');?>
+									<?=($sets['user']['rights'] == 2 ? $comment['cookie'].'&nbsp;'.$comment['ip'].'&nbsp;' : '');?>
 									<img src="/images/comment_edit.png" alt="редактировать" rel="<?=$comment['id'];?>" class="edit_comment">
 									&nbsp;&nbsp;&nbsp;
 									<img src="/images/comment_delete.png" alt="удалить" rel="<?=$comment['id'];?>" class="delete_comment">
