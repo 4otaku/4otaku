c3be80ee737e4c03e972b526524ce90ff1522dc5
derinov <derinov@derinov.(none)>
2010-12-09 01:04:12 +0300

    Some more output

--- templates/main/board/thread.php
+++ templates/main/board/thread.php
@@ -1,1 +1,10 @@
-Приветственное слово
+<? foreach ($data['main']['posts'] as $key => $post) { ?>
+	<? if (empty($key)) { ?>
+		<? 
+			$thread = $post; $id = $post['id'];
+			include TEMPLATE_DIR . '/main/board/thread_message.php'; 
+		?>
+	<? } else { ?>
+		<? include TEMPLATE_DIR . '/main/board/message.php'; ?>
+	<? } ?>
+<? } ?>
