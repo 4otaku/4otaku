2e1e01e7c97db74560b8321dd56f0c22d9eb4597
derinov <derinov@derinov.(none)>
2010-12-13 23:42:24 +0300

    Борда, возможность вывести ленту всех тредов

--- templates/main/board/message.php
+++ templates/main/board/message.php
@@ -1,22 +1,40 @@
 <div class="message" id="board-<?=$post['id'];?>">	
-	<span class="link_reply"><a href="<?=($url[3] != 'thread' ? '/board/'.$url[2].'/thread/'.$id.'#reply-'.$post['id'] : "javascript:add_text('>>".$post['id']."')");?>">Ответить</a></span>
-	<span class="link_delete"><? if ($post['cookie'] && $_COOKIE['settings'] === $post['cookie']) { ?>
-				 <img src="/images/comment_delete.png" alt="Удалить" rel="<?=$post['id'];?>" class="delete_from_board">
-			<? } ?></span>
-	<span class="author"><?=$post['name'];?></span>
-	<? if (!empty($post['trip'])) { ?><span class="trip"><?=$post['trip'];?></span><? } ?>
-	<span class="number"><a href="<?='/board/'.$url[2].'/thread/'.$id.'/#board-'.$post['id'];?>">#<?=$post['id'];?></a></span>
-	<span class="date"><?=$post['pretty_date'];?></span>
+	<span class="link_reply">
+		<a href="<?=($url[3] != 'thread' ? '/board/'.$url[2].'/thread/'.$id.'#reply-'.$post['id'] : "javascript:add_text('>>".$post['id']."')");?>">
+			Ответить
+		</a>
+	</span>
+	<span class="link_delete">
+		<? if ($post['cookie'] && $_COOKIE['settings'] === $post['cookie']) { ?>
+			 <img src="/images/comment_delete.png" alt="Удалить" rel="<?=$post['id'];?>" class="delete_from_board">
+		<? } ?>
+	</span>
+	<span class="author">
+		<?=$post['name'];?>
+	</span>
+	<? if (!empty($post['trip'])) { ?>
+		<span class="trip">
+			<?=$post['trip'];?>
+		</span>
+	<? } ?>
+	<span class="number">
+		<a href="<?='/board/'.$url[2].'/thread/'.$id.'/#board-'.$post['id'];?>">
+			#<?=$post['id'];?>
+		</a>
+	</span>
+	<span class="date">
+		<?=$post['pretty_date'];?>
+	</span>
 	<div class="tbody">
-			<? if ($post['image']) { ?>
-				<a href="/images/board/full/<?=$post['image'][1];?>" target="_blank">
-					<img align="left" src="/images/board/thumbs/<?=$post['image'][2];?>" rel="/images/board/full/<?=$post['image'][1];?>">
-				</a>
-			<? } elseif ($post['video']) { ?>
-				<div class="video"><?=$post['video'];?></div>
-			<? } ?>
-			<div class="posttext">
-				<?=$post['text'];?>
-			</div>
+		<? if ($post['image']) { ?>
+			<a href="/images/board/full/<?=$post['image'][1];?>" target="_blank">
+				<img align="left" src="/images/board/thumbs/<?=$post['image'][2];?>" rel="/images/board/full/<?=$post['image'][1];?>">
+			</a>
+		<? } elseif ($post['video']) { ?>
+			<div class="video"><?=$post['video'];?></div>
+		<? } ?>
+		<div class="posttext">
+			<?=$post['text'];?>
+		</div>
 	</div>
 </div>
--- templates/main/board/thread_message.php
+++ templates/main/board/thread_message.php
@@ -1,30 +1,54 @@
 <div class="thread" id="board-<?=$id;?>">	
-	<span class="link_reply"><a href="<?=($url[3] != 'thread' ? '/board/'.$url[2].'/thread/'.$id.'#reply' : "javascript:add_text('>>".$id."')");?>">Ответить</a></span>
-	<span class="link_read"><a href="/board/<?=$url[2];?>/thread/<?=$id;?>/">Читать</a></span>
-	<? if ($thread['cookie'] && $_COOKIE['settings'] === $thread['cookie']) { ?><span class="link_delete">
-				 <img src="/images/comment_delete.png" alt="удалить" rel="<?=$id;?>" class="delete_from_board">
-			</span><? } ?>
-	<span class="author"><?=$thread['name'];?></span>
-	<? if (!empty($thread['trip'])) { ?><span class="trip"><?=$thread['trip'];?></span><? } ?>
-	<span class="number"><a href="/board/<?=$url[2];?>/thread/<?=$id;?>/">#<?=$id;?></a> в разделе <? foreach($thread['boards'] as $key => $board) { ?>
-				<? if (!empty($key)) { ?>
-					/
-				<? } ?>
-				<a href="/board/<?=$board;?>/">
-					<?=$board;?>
-				</a>
-			<? } ?></span>
-	<span class="date"><?=$thread['pretty_date'];?></span>
-	<div class="tbody">
-			<? if ($thread['image']) { ?>
-				<a href="/images/board/full/<?=$thread['image'][1];?>" target="_blank">
-					<img src="/images/board/thumbs/<?=$thread['image'][2];?>" rel="/images/board/full/<?=$thread['image'][1];?>">
-				</a>	
-			<? } elseif ($thread['video']) { ?>
-				<div class="video"><?=$thread['video'];?></div>
+	<span class="link_reply">
+		<a href="<?=($url[3] != 'thread' ? '/board/'.$url[2].'/thread/'.$id.'#reply' : "javascript:add_text('>>".$id."')");?>">
+			Ответить
+		</a>
+	</span>
+	<span class="link_read">
+		<a href="/board/<?=$url[2];?>/thread/<?=$id;?>/">
+			Читать
+		</a>
+	</span>
+	<? if ($thread['cookie'] && $_COOKIE['settings'] === $thread['cookie']) { ?>
+		<span class="link_delete">
+			 <img src="/images/comment_delete.png" alt="удалить" rel="<?=$id;?>" class="delete_from_board">
+		</span>
+	<? } ?>
+	<span class="author">
+		<?=$thread['name'];?>
+	</span>
+	<? if (!empty($thread['trip'])) { ?>
+		<span class="trip">
+			<?=$thread['trip'];?>
+		</span>
+	<? } ?>
+	<span class="number">
+		<a href="/board/<?=$url[2];?>/thread/<?=$id;?>/">
+			#<?=$id;?>
+		</a>
+		 в разделе 
+		<? foreach($thread['boards'] as $key => $board) { ?>
+			<? if (!empty($key)) { ?>
+				/
 			<? } ?>
-			<div class="posttext">
-				<?=$thread['text'];?>
-			</div>
+			<a href="/board/<?=$board;?>/">
+				<?=$board;?>
+			</a>
+		<? } ?>
+	</span>
+	<span class="date">
+		<?=$thread['pretty_date'];?>
+	</span>
+	<div class="tbody">
+		<? if ($thread['image']) { ?>
+			<a href="/images/board/full/<?=$thread['image'][1];?>" target="_blank">
+				<img src="/images/board/thumbs/<?=$thread['image'][2];?>" rel="/images/board/full/<?=$thread['image'][1];?>">
+			</a>	
+		<? } elseif ($thread['video']) { ?>
+			<div class="video"><?=$thread['video'];?></div>
+		<? } ?>
+		<div class="posttext">
+			<?=$thread['text'];?>
+		</div>
 	</div>
 </div>
