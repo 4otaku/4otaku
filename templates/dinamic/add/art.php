41b0ca606aadf294d6e7903422a9ff3775a49077
derinov <derinov@derinov.(none)>
2010-12-25 21:14:09 +0300

    Просьба тегать в добавлялке арта

--- templates/dinamic/add/art.php
+++ templates/dinamic/add/art.php
@@ -39,13 +39,21 @@
 				<td colspan="2">
 					
 				</td>
-			</tr>
+			</tr>			
 			<tr>
 				<td class="input field_name">
 					Теги
 				</td>
 				<td class="inputdata">
 					<input size="65%" name="tags" value="" type="text">
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					Пожалуйста, тегайте заливаемые вами картинки, хотя бы частично. 
+					<a href="http://wiki.4otaku.ru/%D0%98%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%86%D0%B8%D1%8F_%D0%BF%D0%BE_%D1%82%D0%B5%D0%B3%D0%B0%D0%BC#.D0.94.D0.BB.D1.8F_.D0.BF.D0.BE.D0.BB.D1.8C.D0.B7.D0.BE.D0.B2.D0.B0.D1.82.D0.B5.D0.BB.D0.B5.D0.B9" target="_blank">
+						Инструкция по тегам
+					</a>.
 				</td>
 			</tr>
         </tbody>		
