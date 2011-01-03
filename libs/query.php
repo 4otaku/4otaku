ac9ef133e47de3f2104bce57606fa5b1ed6274d2
Александр <alexander@pakshaev-desktop.(none)>
2011-01-03 13:55:32 +0300

    query, получение post и get

--- libs/query.php
+++ libs/query.php
@@ -9,6 +9,73 @@ class query
 	static public $post = array();
 	static public $get = array();
 	
+	static private $safe_replacements = array(
+		'&' => '&amp;',
+		'"' => '&quot;',
+		'<' => '&lt;',
+		'>' => '&gt;',
+		'\\' => '&#092;',
+		"'" => '&apos;',
+		'⟯' => '',
+	);
+	
+	static function get_globals($get, $post) {
+		self::clean_globals($get);
+		self::clean_globals($post);
+		self::$get = self::parse_incoming_recursively($get, array());
+		self::$post = self::parse_incoming_recursively($post, array());
+
+		if (isset($post['remember'])) {
+			$md5 = md5(serialize($post));
+			if (obj::db()->sql('select id from input_filter where md5 = "'.$md5.'"',2)) unset($post);
+			else obj::db()->insert('input_filter',array($md5,time()));
+		}		
+		
+		unset ($_GET, $_POST);
+		return array(self::$get, self::$post);
+	}
+
+	static function clean_globals(&$data,$iteration = 0) {
+		if ($iteration > 10) {
+			return;
+		}
+
+		foreach ($data as $k => $v)	{
+			if (is_array($v)) {
+				self::clean_globals($data[$k],$iteration + 1);
+			} else {
+				$v = str_replace(chr('0'),'',$v);
+				$v = str_replace("\0",'',$v);
+				$v = str_replace("\x00",'',$v);
+				$v = str_replace('%00','',$v);
+				$v = str_replace("../","&#46;&#46;/",$v);
+				$data[$k] = stripslashes($v);
+			}
+		}
+	}
+
+	function parse_incoming_recursively(&$data,$input,$iteration = 0) {
+		if ($iteration > 10) {
+			return $input;
+		}
+
+		foreach($data as $k => $v) {
+			if (is_array($v)) {
+				$input[$k] = self::parse_incoming_recursively($data[$k], array(), $iteration + 1);
+			} else {
+				$k = str_replace(
+					array_keys(self::$safe_replacements),
+					array_values(self::$safe_replacements),
+					$k);
+				$v = str_replace(
+					array_keys(self::$safe_replacements),
+					array_values(self::$safe_replacements),
+					$v);
+				$input[$k] = $v;
+			}
+		}
+
+		return $input;
+	}	
 	
 }
-?>
