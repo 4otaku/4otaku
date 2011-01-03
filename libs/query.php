07bdc150b44d31e5e6f38a7691e30f1f3d9c08ac
Александр <alexander@pakshaev-desktop.(none)>
2011-01-03 13:02:07 +0300

    Создал заготовку под класс query

---
+++ libs/query.php
@@ -0,0 +1,14 @@
+<?
+
+class query
+{	
+	static public $url = array();
+	static public $class;
+	static public $type;
+	static public $vars = array();
+	static public $post = array();
+	static public $get = array();
+	
+	
+}
+?>
