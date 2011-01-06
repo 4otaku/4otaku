f867948c312a21f1d0cd6081a6170a4fc815c9d5
unknown <derinov@.(none)>
2011-01-06 15:07:10 +0300

    fix

--- go/index.php
+++ go/index.php
@@ -1,7 +1,7 @@
 <?
 $request = preg_replace('/^\/go\/?(\?|\/index\.php\?)?/i', '', urldecode($_SERVER['REQUEST_URI']));
 
-if(!$_GET) die;
+//if(!$_GET) die;
 if (preg_match_all('/(http|https|ftp):\/\/([A-Z0-9][A-Z0-9_-]*(?:\.[A-Z0-9][A-Z0-9_-]*)+):?(\d+)?\/?(.*)?$/iD', $request, $matches) !== 1
 && preg_match('/[^a-zа-яё\d_\-\+%&\.,=\/]/iu', $request))	die;
 
