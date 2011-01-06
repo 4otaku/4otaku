049a4b8037126e60cfe429e91d6b9a5ccd54754b
unknown <derinov@.(none)>
2011-01-06 10:48:04 +0300

    fix an old misspeling

---
+++ libs/dynamic/add.php
@@ -0,0 +1,66 @@
+<? 
+class dynamic__add
+{
+	function art() {
+		global $sets;
+		$sets['user']['name'] = preg_replace('/#.*$/','',$sets['user']['name']);
+		$return['category'] = obj::db()->sql('select alias, name from category where locate("|art|",area) order by id','alias');
+		return $return;
+	}	
+	
+	function video() {
+		global $sets;
+		$sets['user']['name'] = preg_replace('/#.*$/','',$sets['user']['name']);
+		$return['category'] = obj::db()->sql('select alias, name from category where locate("|video|",area) order by id','alias');
+		return $return;
+	}
+	
+	function post() {
+		global $sets;
+		$sets['user']['name'] = preg_replace('/#.*$/','',$sets['user']['name']);
+		$return['category'] = obj::db()->sql('select alias, name from category where locate("|post|",area) order by id','alias');
+		$return['language'] = obj::db()->sql('select alias, name from language order by id','alias');
+		return $return;
+	}		
+	
+	function board() {
+		$return['category'] = obj::db()->sql('select alias, name from category where locate("|board|",area) order by id','alias');
+		return $return;
+	}
+	
+	function order() {
+		global $sets;
+		$sets['user']['name'] = preg_replace('/#.*$/','',$sets['user']['name']);
+		$return['category'] = obj::db()->sql('select alias, name from category where !locate("|board|",area) order by id','alias');
+		return $return;
+	}
+	
+	function replay() {
+		return true;
+	}				
+
+	function comment() {
+		global $sets;
+		$sets['user']['name'] = preg_replace('/#.*$/','',$sets['user']['name']);	
+		return true;
+	}	
+
+	function pool() {
+		return true;
+	}	
+
+	function update() {
+		global $get; global $check;
+		if ($check->num($get['id'])) {
+			$links = obj::db()->sql('select link from post where id='.$get['id'],2);
+		}		
+		return unserialize($links);
+	}
+	
+	function checkpassword() {
+		global $get; global $check;
+		if (!$check->num($get['id'])) echo 'fail';
+		elseif (obj::db()->sql('select password from art_pool where id='.$get['id'],2) == md5($get['val'])) echo 'success';
+		else echo 'fail';
+	}
+}
---
+++ libs/dynamic/post.php
@@ -0,0 +1,12 @@
+<?
+
+class dynamic__post
+{
+	function show_updates() { 	
+		global $check; global $get;
+		if ($check->num($get['id']))
+			$return = obj::db()->sql('select * from updates where post_id='.$get['id']);
+		foreach ($return as &$update) $update['link'] = unserialize($update['link']);
+		return $return;
+	}
+}
