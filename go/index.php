64bcca890357ae0db076203fe476012e75122e95
derinov <derinov@derinov.(none)>
2011-01-12 08:28:26 +0300

    fixes

--- go/index.php
+++ go/index.php
@@ -3,7 +3,7 @@ $request = preg_replace('/^\/go\/?(\?|\/index\.php\?)?/i', '', urldecode($_SERVE
 
 //if(!$_GET) die;
 if (preg_match_all('/(http|https|ftp):\/\/([A-Z0-9][A-Z0-9_-]*(?:\.[A-Z0-9][A-Z0-9_-]*)+):?(\d+)?\/?(.*)?$/iD', $request, $matches) !== 1
-&& preg_match('/[^a-zа-яё\d_\-\+%&\.,=\/]/iu', $request))	die;
+|| preg_match('/[^a-zа-яё\d_\-\+%&\.,=\/]/iu', $request))	die;
 
 $link = str_replace('"','\\"',$matches[0][0] ? $matches[0][0] : $request);
 
