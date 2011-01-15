38d9cba2dd522422342d2371c1b2a21527c4473e
derinov <derinov@derinov.(none)>
2011-01-15 22:36:35 +0300

    fix

--- libs/cron.php
+++ libs/cron.php
@@ -128,7 +128,7 @@ class cron
 				if ($id = obj::db()->sql('select id from orders where (id ='.$delete['data2'].' and area = "workshop")',2)) {
 					obj::db()->sql('delete from misc where id ='.$delete['id'],0);
 					obj::db()->sql('update orders set area = "flea_market", comment_count=comment_count+1, last_comment='.($time = ceil(microtime(true)*1000)).' where id='.$id,0);
-					obj::db()->insert('comment',array(0,0,'orders',$id,'Gouf Custom MS-07B-3','gouf@4otaku.ru','255.255.255.255',
+					obj::db()->insert('comment',array(0,0,'orders',$id,'Gouf Custom MS-07B-3','gouf@4otaku.ru','255.255.255.255','dummy',
 						'Закрыл автоматом за долгое отсутствие интереса и прогресса','Закрыл автоматом за долгое отсутствие интереса и прогресса',
 						obj::transform('text')->rudate(),$time,'workshop'));
 				}
