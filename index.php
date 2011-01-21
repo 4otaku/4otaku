74da91c70d426609513a1e14af9479b350c91c70
Дмитрий <shtrih.org.ru@mail.ru>
2011-01-20 20:05:41 -0800

    "красивый код"　fix

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 
 include_once 'inc.common.php';
 
-$request = preg_replace('/^'.preg_quote(SITE_DIR,'/').'/','',$_SERVER["REQUEST_URI"]);
-$url = explode('/',preg_replace('/\?[^\/]+$/','',$request)); 
+$request = preg_replace('/^'.preg_quote(SITE_DIR,'/').'/', '', $_SERVER["REQUEST_URI"]);
+$url = explode('/', preg_replace('/\?[^\/]+$/', '', $request)); 
 
-if(isset($url[0])) unset($url[0]);
-if(empty($url[1])) $url[1] = 'index';
+if (isset($url[0])) 
+	unset($url[0]);
+if (empty($url[1]))
+	$url[1] = 'index';
 
 include_once ROOT_DIR.SL.'engine'.SL.'handle_old_urls.php';
 
@@ -28,21 +30,24 @@ if ($url[1] == 'confirm' || $url[1] == 'stop_emails') {
 if (isset(query::$post['do'])) {
 	query::$post['do'] = explode('.',query::$post['do']);
 	if (count(query::$post['do']) == 2) {
-		$input_class = 'input__'.query::$post['do'][0]; $input = new $input_class;
-		$input_function = query::$post['do'][1]; $input->$input_function(query::$post);		
+		$input_class = 'input__'.query::$post['do'][0];
+		$input = new $input_class;
+		$input_function = query::$post['do'][1];
+		$input->$input_function(query::$post);		
 	}	
 	$redirect = 'http://'.def::site('domain').(empty($input->redirect) ? $_SERVER["REQUEST_URI"] : $input->redirect);
 	engine::redirect($redirect);
 } else {
 	$data = array();
 
-	$output_class = 'output__'.$url[1]; $output = new $output_class;
+	$output_class = 'output__'.$url[1];
+	$output = new $output_class;
 
-	$output->check_404($output->allowed_url); 
+	$output->check_404($output->allowed_url);
 	if (!$error) 
 		$data['main'] = $output->get_data();
 		
-	$data = array_merge($data,$output->get_side_data($output->side_modules));
+	$data = array_merge($data, $output->get_side_data($output->side_modules));
 	if ($error) 
 		$output->make_404($output->error_template);
 
