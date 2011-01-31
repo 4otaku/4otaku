fe728ea45cfdd18bd51cc798f5ab0a5aa2b54c57
derinov <derinov@derinov.(none)>
2011-01-31 22:33:35 +0300

    Разделил кронтаски на быстрые и медленные

--- cron.php
+++ cron.php
@@ -1,18 +1,28 @@
+#!/usr/bin/php
 <?
 
 include_once 'inc.common.php';
 
 $cron = new cron();
 
+if (in_array('slow', $argv)) {
+	$time = time();
+	$tasks = obj::db()->sql('select * from cron where time < '.$time.' and period >= 5400');
+	set_time_limit(1800);
+	ini_set('memory_limit', '1024M');
+}
+
 if ($key = key($_GET)) {
 	$cron->$key();
 	var_dump(memory_get_peak_usage(true));
+	exit();
 } else {
 	$time = time();
-	$tasks = obj::db()->sql('select * from cron where time < '.$time);
-	if (is_array($tasks)) foreach ($tasks as $task) if (method_exists($cron,$task['function'])) {
-		$func = $task['function'];
-		$cron->$func(); $nexttime = $time + $task['period'] - 15;
-		obj::db()->update('cron','time',$nexttime,$task['id']);
-	}
+	$tasks = obj::db()->sql('select * from cron where time < '.$time.' and period < 5400');
+}
+
+if (is_array($tasks)) foreach ($tasks as $task) if (method_exists($cron,$task['function'])) {
+	$func = $task['function'];
+	$cron->$func(); $nexttime = $time + $task['period'] - 15;
+	obj::db()->update('cron','time',$nexttime,$task['id']);
 }
