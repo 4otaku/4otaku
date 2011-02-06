ee1b889114f2d0b3c2d3f88c3789e88c50b78a88
4otaku <noname-nya@yandex.ru>
2011-02-06 20:30:50 +0300

    fix

--- templates/main/booru/page.php
+++ templates/main/booru/page.php
@@ -1,3 +1,12 @@
+<? if ($url[2] == def::get('area',2)) { ?>
+	<div class="mini-shell">
+		Графические ресурсы для создания любительских ВН. 
+		<a href="http://wiki.4otaku.ru/%D0%A1%D0%BE%D0%B7%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5_%D0%BB%D1%8E%D0%B1%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%BE%D0%B9_%D0%92%D0%9D">
+			Статья про создание
+		</a> 
+		(в процессе написания).
+	</div>
+<? } ?>
 <div class="booru_images">
 	<?  
 		if (is_array($data['main']['art']['thumbs'])) foreach ($data['main']['art']['thumbs'] as $key => $picture) {
