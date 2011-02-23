aefc92c34adb6b03dc75c81ab2518a0ec7786654
4otaku <noname-nya@yandex.ru>
2011-02-23 22:18:24 +0300

    fix

--- dump/mysql.sql
+++ dump/mysql.sql
@@ -107,7 +107,7 @@ CREATE TABLE IF NOT EXISTS `{pr}art_similar` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `{pr}art_pool`;
-CREATE TABL `{pr}art_pool` (
+CREATE TABLE `{pr}art_pool` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `text` text NOT NULL,
