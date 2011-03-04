7e485809e6d9be01345c2152c72857c016eb2918
4otaku <noname-nya@yandex.ru>
2011-03-04 06:27:39 +0300

    Расширил postprocess

--- engine/view/manager.php
+++ engine/view/manager.php
@@ -24,49 +24,8 @@ class Manager
 	public function postprocess() {
 		
 		if (!empty($this->data['pagecount']) && !empty($this->data['curr_page'])) {			
-			$this->data['navi'] = array();
-			
-			$base = '/post/';
-			
-			$radius = 5;
-			$low_end = max($this->data['curr_page'] - $radius, 2);
-			$high_end = min($this->data['curr_page'] + $radius, $this->data['pagecount'] - 1);
-			
-			for ($i = 1; $i <= $this->data['pagecount']; $i++) {
-				
-				$inside = ($i <= $high_end && $i >= $low_end);
-				
-				if ($i == 1 || $inside || $i == $this->data['pagecount']) {
-					if ($i == $this->data['curr_page']) {
-						$this->data['navi'][$i] = array('type' => 'active');
-					} else {
-						$this->data['navi'][$i] = array('type' => 'enabled');
-					}
-					
-					if ($i == 1) {
-						$this->data['navi'][$i]['url'] = $base;
-					} else {
-						$this->data['navi'][$i]['url'] = $base.'page/'.$i.'/';
-					}
-				} else {
-					$this->data['navi'][$i] = array('type' => 'skip');
-					if ($i > $high_end) {
-						$i = $this->data['pagecount'] - 1;
-					} else {
-						$i = $low_end - 1;
-					}
-				}
-			}
-			
-			if ($this->data['curr_page'] == 2) {
-				$this->data['navi_back'] = $base;
-			} elseif ($this->data['curr_page'] > 2) {
-				$this->data['navi_back'] = $base.'page/'.($this->data['curr_page'] - 1).'/';
-			}
-			
-			if ($this->data['curr_page'] < $this->data['pagecount']) {
-				$this->data['navi_forward'] = $base.'page/'.($this->data['curr_page'] + 1).'/';
-			}
+			$worker = new Process_Navi();
+			$this->data = $worker->process($this->data);
 		}
 	}
 	
