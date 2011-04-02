278ed8d640ccc3d64220f8db49141bd3f2ce0ad0
4otaku <noname-nya@yandex.ru>
2011-04-02 15:24:46 +0400

    дополнил autoloader

--- engine/autoloader.php
+++ engine/autoloader.php
@@ -20,9 +20,29 @@
 	}
 	
 	function autoload_normal ($name) {
+		$name = str_replace('_', SL, strtolower($name)).'.php';
+
 		if ($library = search_lib($name)) {
 			include_once($library);
+			return true;
+		}
+		
+		$alt_name = preg_replace('/^(.+)\\'.SL.'(.+?)$/', '$1_$2', $name);
+		
+		if ($library = search_lib($alt_name)) {
+			include_once($library);
 			return true;			
+		}		
+		
+		return false;
+	}	
+	
+	function autoload_factory ($name) {
+		$name = str_replace('_', '/', strtolower($name)).SL.'_factory.php';
+
+		if ($library = search_lib($name)) {
+			include_once($library);
+			return true;
 		}
 		
 		return false;
@@ -35,18 +55,11 @@
 			ENGINE.SL.'libs'.SL,
 		);
 
-		$name = str_replace('_', '/', strtolower($name));
-		$alt_name = preg_replace('/^(.+)\/(.+?)$/', '$1_$2', $name);
-		
 		foreach ($directories as $directory) {
 
-			if (is_readable($directory.SL.$name.'.php')) {
-				return $directory.SL.$name.'.php';
+			if (is_readable($directory.SL.$name)) {
+				return $directory.SL.$name;
 			}
-
-			if (is_readable($directory.SL.$alt_name.'.php')) {
-				return $directory.SL.$alt_name.'.php';
-			}			
 		}
 		
 		return false;
@@ -56,3 +69,4 @@
 	// может потребоваться добавить свой autoload
 	spl_autoload_register('autoload_extended', false);
 	spl_autoload_register('autoload_normal', false);
+	spl_autoload_register('autoload_factory', false);
