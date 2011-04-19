f673da368df9e8cdfeb296a6bb3c6aae6498c6a7
4otaku <noname-nya@yandex.ru>
2011-04-19 21:27:19 +0400

    таблица и папки под файлы борды

--- dump/mysql.sql
+++ dump/mysql.sql
@@ -290,3 +290,24 @@ CREATE TABLE IF NOT EXISTS `<pr>user` (
   KEY `login_name` (`username`,`password`),
   KEY `login_mail` (`email`,`password`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `<pr>board`;
+CREATE TABLE `<pr>board` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `thread` int(10) unsigned NOT NULL,  
+  `sage` tinyint(1) NOT NULL DEFAULT '0',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `name` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `trip` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `cookie` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
+  `pretty_text` text COLLATE utf8_unicode_ci NOT NULL,
+  `text` text COLLATE utf8_unicode_ci NOT NULL,
+  `meta` text COLLATE utf8_unicode_ci NOT NULL,
+  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NULL DEFAULT NULL,  
+  PRIMARY KEY (`id`),
+  KEY `selector` (`thread`,`deleted`,`updated`),
+  KEY `posts` (`thread`,`deleted`,`date`),
+  FULLTEXT KEY `index` (`meta`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
