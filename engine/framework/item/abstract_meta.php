bd4064dd5f153cee1187cfec4c99cd8f8829112e
4otaku <noname-nya@yandex.ru>
2011-04-24 10:41:49 +0400

    Борда, немного прогресса

--- engine/framework/item/abstract_meta.php
+++ engine/framework/item/abstract_meta.php
@@ -37,7 +37,10 @@ abstract class Item_Abstract_Meta extends Item_Abstract_Marked implements Plugin
 		}
 
 		if (!empty($this->data['date'])) {
+						
 			$this->data['date'] = Database::date_to_unix($this->data['date']);
+			
+			$this->data['precise_date'] = Transform_String::rudate($this->data['date'], true);
 			$this->data['date'] = Transform_String::rudate($this->data['date']);
 		}
 	}
