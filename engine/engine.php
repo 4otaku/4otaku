97b8148c9ab6966e04b226d9f1e3c03660a8a8cc
4otaku <noname-nya@yandex.ru>
2011-09-12 23:24:22 +0400

    Я свободен от того, чтобы писать код сразу правильно

--- engine/engine.php
+++ engine/engine.php
@@ -3,67 +3,119 @@
 class engine
 {
 	public $error_template = 'main';
-	
-	function parse_area() {
-		global $url; global $def;
-		if (in_array($url[2],$def['area'])) {
-			$area = $url[2];
-			foreach ($url as $key => $one) if ($key > 2) $url[$key-1] = $one;
-			$url = array_slice($url,0,-1,true); $url['area'] = $area;
-		}
-		else $url['area'] = $def['area'][0];
-		if ($url[2] == 'tag') $url['tag'] = obj::db()->sql('select alias from tag where locate("|'.mysql_real_escape_string(urldecode($url[3])).'|",variants)',2);
-	}	
 
-	function check_404($ways) {
-		global $url; global $error;
-		$error = true; 
-		foreach ($ways as $conditions) {
-			$error = false; 
-			foreach ($conditions as $key => $condition) {
-				if (!empty($url[$key])) {
-					if (preg_match("/[^a-zа-яё\d_\-\+%&\.,=:]/iu",$url[$key])) $error = true;					 
-					elseif ($condition == 'end') $error = true;
-					elseif ($condition == 'num') { if (!is_numeric($url[$key]) && $url[$key]) $error = true; }
-					elseif (!strpos(' '.$condition,'|'.$url[$key].'|') && $url[$key] && $condition != 'any') $error = true;
+	function parse_area () {
+		global $url;
+
+		if (in_array($url[2], def::get('area'))) {
+
+			$area = $url[2];
+			foreach ($url as $key => $one) {
+				if ($key > 2) {
+					$url[$key-1] = $one;
 				}
-				if ($error) break;
 			}
-			if (!$error) break;
+			$url = array_slice($url, 0, -1, true);
+			$url['area'] = $area;
+		} else {
+			$url['area'] = def::get('area', 0);
+		}
+
+		if ($url[2] == 'tag') {
+			$locate = "|$url[3]|";
+			$url['tag'] = Database::get_field('tag', 'alias', 'locate(?,variants)', $locate);
 		}
 	}
-	
-	function make_404($type = false) {
-		if (!empty($type)) $this->template = '404__'.$type;
+
+	function check_404 ($ways) {
+		global $url; global $error;
+
+		$error = true;
+		foreach ($ways as $conditions) {
+
+			$error = false;
+			foreach ($conditions as $key => $condition) {
+
+				if (!empty($url[$key])) {
+					if (preg_match("/[^a-zа-яё\d_\-\+%&\.,=:]/iu", $url[$key])) {
+						$error = true;
+					} elseif ($condition == 'end') {
+						$error = true;
+					} elseif ($condition == 'num') {
+						if (!is_numeric($url[$key]) && $url[$key]) {
+							$error = true;
+						}
+					} elseif (
+						!strpos(' '.$condition,'|'.$url[$key].'|') &&
+						$url[$key] && $condition != 'any'
+					) {
+						$error = true;
+					}
+				}
+				if ($error) {
+					break;
+				}
+			}
+			if (!$error) {
+				break;
+			}
+		}
+	}
+
+	function make_404 ($type = false) {
+		if (!empty($type)) {
+			$this->template = '404__'.$type;
+		}
 		self::error_headers();
 	}
-	
-	static function error_headers() {
-		header("Expires: " . gmdate("D, d M Y H:i:s") . " GMT");
-		header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+
+	static function error_headers () {
+		header("Expires: ".gmdate("D, d M Y H:i:s")." GMT");
+		header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
 		header("Cache-Control: no-store, no-cache, must-revalidate");
 		header("Pragma: no-cache");
 		header("HTTP/1.x 404 Not Found");
 	}
 
-	function get_side_data($input) {
+	function get_side_data ($input) {
 		$return = array();
+
 		foreach ($input as $part => $modules) {
 			$class = 'side__'.$part;
-			$$part = new $class();
-			foreach ($modules as $module) $return[$part][$module] = $$part->$module();
+			$worker = new $class();
+			foreach ($modules as $module) {
+				$return[$part][$module] = $worker->$module();
+			}
 		}
 		return $return;
 	}
-	
-	function get_meta($rows,$tables) {
-		foreach ($rows as $row) 
-			foreach ($tables as $table)
+
+	function get_meta ($rows, $tables) {
+
+		$alias = array_fill_keys($tables, '');
+		foreach ($rows as $row) {
+			foreach ($tables as $table) {
 				$alias[$table] .= $row[$table];
+			}
+		}
+
+		$return = array_fill_keys($tables, 0);
 		foreach ($tables as $table) {
-			if ($table != 'tag') $return[$table] = obj::db()->sql('select alias, name from '.$table.' where alias="'.implode('" or alias="',array_unique(array_filter(explode('|',$alias[$table])))).'"','alias');
-			else {
-				$return[$table] = obj::db()->sql('select alias, name, color, variants, have_description from '.$table.' where alias="'.implode('" or alias="',array_unique(array_filter(explode('|',$alias[$table])))).'"','alias');
+			$aliases = array_unique(array_filter(explode('|', $alias[$table])));
+			if ($table != 'tag') {
+
+				$return[$table] = Database::get_vector($table,
+					array('alias', 'name'),
+					Database::array_in('alias', $aliases),
+					$aliases
+				);
+			} else {
+
+				$return[$table] = Database::get_vector($table,
+					array('alias', 'name', 'color', 'variants', 'have_description'),
+					Database::array_in('alias', $aliases),
+					$aliases
+				);
 				if (!empty($return[$table])) {
 					foreach ($return[$table] as &$one) {
 						$one['variants'] = array_filter(explode('|',trim($one['variants'],'|')));
@@ -71,9 +123,9 @@ class engine
 				}
 			}
 		}
-		return $return;	
+		return $return;
 	}
-	
+
 	static function have_tag_variants($tags) {
 		if (is_array($tags)) {
 			foreach ($tags as $tag) {
@@ -82,40 +134,77 @@ class engine
 				}
 			}
 		}
+
 		return false;
 	}
-	
-	function get_comments($place, $id, $pos = false) {
-		global $sets;
-		$return['number'] = obj::db()->sql('select count(id) from comment where (place="'.$place.'" and post_id="'.$id.'" and rootparent=0 and area != "deleted")',2,'count(id)');
-		if ($return['number']) { 
-			if ($pos === false) $limit='';
-				else $limit = ' limit '.($sets['pp']['comment_in_post']*($pos - 1)).', '.$sets['pp']['comment_in_post'];
-			if ($sets['dir']['comments_tree']) $order=' desc'; 
-				else $order = '';
-			$parents = obj::db()->sql('select * from comment where (place="'.$place.'" and post_id="'.$id.'" and rootparent=0 and area != "deleted") order by sortdate'.$order.$limit);
+
+	function get_comments ($place, $id, $pos = false) {
+		$return = array();
+
+		if ($pos === false) {
+			$limit = '';
+		} else {
+			$limit = ' limit '.(sets::pp('comment_in_post')*($pos - 1)).
+				', '.sets::pp('comment_in_post');
+		}
+
+		if ($sets['dir']['comments_tree']) {
+			$order = ' desc';
+		} else {
+			$order = '';
+		}
+
+		$condition = 'place = ? and post_id= ? and rootparent = ? '.
+			'and area != ? order by sortdate'.$order.$limit;
+		$params = array($place, $id, 0, "deleted");
+
+		$parents = Database::set_counter()->get_full_vector('comment', $condition, $params);
+		$children = array();
+
+		$return['number'] = Database::get_counter();
+		if (!empty($return['number'])) {
+
 			if (!empty($parents)) {
-				foreach ($parents as $parent) $condition .= ' or rootparent='.$parent['id'];
-				$temp_children = obj::db()->sql('select * from comment where (('.substr($condition,4).') and area != "deleted") order by sortdate'.$order);
-				if (is_array($temp_children)) foreach ($temp_children as $child) $children[$child['rootparent']][$child['id']] = $child;
-				foreach ($parents as $parent) {
-					 if (is_array($children[$parent['id']]))
-						$return['comments'][] = $this->get_comments_tree($parent,$children[$parent['id']],array());
-					else 
-						$return['comments'][] = array_merge($parent,array('position' => 0));
+
+				$params = array_keys($parents);
+				$condition = Database::array_in('rootparent', $params).
+					' and area != ? order by sortdate'.$order;
+				array_push($params, "deleted");
+				$temp_children = Database::get_full_table('comment', $condition, $params);
+
+				if (!empty($temp_children)) {
+					foreach ($temp_children as $child) {
+						$children[$child['rootparent']][$child['id']] = $child;
+					}
+				}
+
+				foreach ($parents as $id => $parent) {
+					$parent['id'] = $id;
+					if (!empty($children[$id])) {
+						$return['comments'][] = $this->get_comments_tree($parent, $children[$id], array());
+					} else {
+						$return['comments'][] = array_merge($parent, array('position' => 0));
+					}
 				}
 			}
 			return $return;
-		}		
+		}
 	}
-	 
-	function get_comments_tree($root,$children,$return,$position = 0) {
-		$return = array_merge($root,array('position' => $position));
-		if (is_array($children)) foreach ($children as $child) 
-			if ($child['parent'] == $root['id']) $return['children'][] = $this->get_comments_tree($child,$children,$return,($position+1));
+
+	function get_comments_tree ($root, $children, $return, $position = 0) {
+		$return = array_merge($root, array('position' => $position));
+
+		if (is_array($children)) {
+			foreach ($children as $child) {
+				if ($child['parent'] == $root['id']) {
+					$return['children'][] = $this->get_comments_tree($child,$children,$return,($position+1));
+				}
+			}
+		}
+
 		return $return;
 	}
-	
+
 	function make_tip($text) {
 		$text = strip_tags($text,'<br>');
 		$pos = mb_strpos($text,'<br');
@@ -130,7 +219,7 @@ class engine
 		}
 		return $text;
 	}
-	
+
 	static function add_res($text, $error = false, $force_cookie = false) {
 		global $add_res; global $cookie;
 		$add_res = array('text' => $text, 'error' => $error);
@@ -140,7 +229,7 @@ class engine
 			$cookie->inner_set('add_res.error',$error);
 		}
 	}
-	
+
 	static function mixed_parse($string) {
 		global $error; global $mixed;
 		$temp_params = explode('&',str_replace(' ','+',$string));
@@ -148,7 +237,7 @@ class engine
 			$param_part=explode ('=',$param);
 			$params[$param_part[0]] = $param_part[1];
 		}
-		
+
 		$singular = array(0 => 'category',1 => 'tag',2 => 'language',3 => 'author');
 		$plural = array(0 => 'categories',1 => 'tags',2 => 'languages',3 => 'authors');
 
@@ -172,7 +261,7 @@ class engine
 	}
 
 	static function mixed_make_sql($mixed) {
-		global $url;	
+		global $url;
 		$return = 'area="'.$url['area'].'"';
 		$plusing = array('categories' => 'category','tags' => 'tag','languages' => 'language','authors' => 'author');
 
@@ -184,20 +273,20 @@ class engine
 					$return .= ' and '.($one['sign'] == "+" ? '' : '!').'( ';
 					foreach ($one['data'] as $or => $part) $return .= ($or ? 'or' : '').' locate("|'.$part.'|",'.$key.') ';
 					$return .= ')';
-				}			
+				}
 			}
 		}
 		return $return;
-	}	
-	
+	}
+
 	static function mixed_make_url($mixed) {
 		global $url; global $def;
-		
+
 		if ($url['area'] != $def['area'][0]) $base = '/'.$url[1].'/'.$url['area'].'/';
 		else $base = '/'.$url[1].'/';
 
 		$plusing = array('categories' => 'category','tags' => 'tag','languages' => 'language','authors' => 'author');
-		
+
 		if (count($mixed) == 0) return $base;
 
 		if (count($mixed) == 1) {
@@ -214,12 +303,12 @@ class engine
 			foreach ($param as $one) {
 				if ($one['sign'] == '+' && $first) $one['sign'] = '';
 				$return .= $one['sign'].implode(',',$one['data']);
-				$first = false;				
+				$first = false;
 			}
 			$return .= '&';
 		}
 		return $base.'mixed/'.substr($return,0,-1).'/';
-	}	
+	}
 
 	function mixed_add($new,$type,$sign = '+') {
 		global $mixed;
@@ -227,7 +316,7 @@ class engine
 
 		$plusing = array('categories' => 'category','tags' => 'tag','languages' => 'language','authors' => 'author');
 		$singplu = array_flip($plusing);
-		
+
 		$temp = $mixed;
 		foreach ($temp as $key => $part) {
 			if ($key == $type || $plusing[$key] == $type || $singplu[$key] == $type) {
@@ -247,11 +336,11 @@ class engine
 			}
 		}
 		$temp[$type][] = array('data' => array($new), 'sign' => $sign);
-		return $this->mixed_make_url($temp);		
-	}	
-	
+		return $this->mixed_make_url($temp);
+	}
+
 	function tag_cloud ($maxsize,$minsize,$area,$words,$limit = false) {
-		if ($limit) $limit =  ' limit '.$limit; 
+		if ($limit) $limit =  ' limit '.$limit;
 		$tags = array('ru' => array(), 'nonru' => array());
 
 		$temp_tags = obj::db()->sql('select alias, name, '.$area.' from tag where ('.$area.' > 0 and alias != "prostavte_tegi") order by '.$area.' desc'.$limit);
@@ -267,7 +356,7 @@ class engine
 				$word = obj::transform('text')->wcase($tag[$area],$words[0],$words[1],$words[2]);
 				if (preg_match('/[а-яА-Я]/u', mb_substr($tag['name'],0,1)))
 					$tags['ru'][str_replace('_','_<wbr />',$name)]= array('alias' => $tag['alias'], 'count' => $tag[$area], 'size' => $calc, 'word' => $word);
-				else 
+				else
 					$tags['nonru'][str_replace('_','_<wbr />',$name)]= array('alias' => $tag['alias'], 'count' => $tag[$area], 'size' => $calc, 'word' => $word);
 			}
 
@@ -275,7 +364,7 @@ class engine
 			return array_merge($tags['ru'],$tags['nonru']);
 		}
 	}
-	
+
 	function make_rss($area,$type,$value) {
 		$name = array('tag' => 'тега', 'author' => 'автора', 'language' => 'языка', 'category' => 'Категории', 'pool' => 'Группы');
 		if ($type == 'pool') $metaname = obj::db()->sql('select name from art_pool where id='.$value,2);
@@ -285,14 +374,14 @@ class engine
 			'meta-name' => $metaname,
 			'area' => $area,
 			'type' => $type,
-			'value' => $value										
+			'value' => $value
 		);
 	}
-	
+
 	function get_navigation($parts) {
 		global $url; global $def;
 		foreach ($parts as $part) {
-			if ($part == 'tag') { 
+			if ($part == 'tag') {
 				if ($url['area'] != $def['area'][2]) $area = $url[1].'_'.$def['area'][0];
 				else $area = $url[1].'_'.$def['area'][2];
 				if ($return[$part] = obj::db()->sql('select alias, name from '.$part.' order by '.$area.' desc limit 70','alias')) {
@@ -305,7 +394,7 @@ class engine
 		}
 		return $return;
 	}
-	
+
 	static function redirect($url, $permanent = false) {
 		$permanent ? header("HTTP/1.x 301 Moved Permanently") : header("HTTP/1.x 302 Moved Temporarily");
 		header("Location: $url");
--- libs/sets.php
+++ libs/sets.php
@@ -3,7 +3,7 @@
 class sets
 {
 	static $data = array();
-	
+
 	static function import($def) {
 		foreach ($def as $key => $value) {
 			self::$data[$key] = $value;
@@ -13,61 +13,65 @@ class sets
 	static function array_get($name) {
 		if (isset(self::$data[$name])) {
 			return self::$data[$name];
-		} 
-		
-		return array();	
+		}
+
+		return array();
 	}
 
 	static function get($name, $key, $def_retrieve = true) {
 		if ($key === false) {
 			return self::array_get($name);
-		}		
-		
+		}
+
 		if (isset(self::$data[$name][$key])) {
 			return self::$data[$name][$key];
-		} 
-		
+		}
+
 		// Если внезапно нужной настройки не оказалось, последние меры умирающего.
 		if (empty(self::$data)) {
 			include ROOT_DIR.SL.'engine'.SL.'config.php';
 			self::import($sets);
 			if (isset($sets[$name][$key])) {
 				return $sets[$name][$key];
-			}			
-		} 
-		
-		if ($def_retrieve && ($def = def::get($name,$key,false))) {
-			return $def;			
-		} 
+			}
+		}
 
-		return null;			
+		if ($def_retrieve && ($def = def::get($name,$key,false))) {
+			return $def;
+		}
+
+		return null;
 	}
-	
+
 	static function set($name, $key, $value) {
 		self::$data[$name][$key] = $value;
-	}	
-	
+	}
+
 	public static function __callStatic($name, $arguments) {
 		return self::get($name,$arguments[0]);
 	}
-	
+
 	static function site($key) {
 		return self::get('site',$key);
-	}	
-	
+	}
+
 	static function video($key) {
 		return self::get('video',$key);
-	}		
-	
+	}
+
 	static function user($key) {
 		return self::get('user',$key);
 	}
-	
+
 	static function board($key) {
 		return self::get('board',$key);
-	}	
-	
+	}
+
 	static function pp($key) {
 		return self::get('pp',$key);
+	}
+
+	static function dir($key) {
+		return self::get('dir',$key);
 	}
 }
