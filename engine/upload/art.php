f0ced889fe54567b6f9f6611b21a320894c55802
4otaku <noname-nya@yandex.ru>
2011-09-12 17:36:27 +0400

    Чотаку сам себя не отрефакторит

--- engine/upload/art.php
+++ engine/upload/art.php
@@ -1,35 +1,43 @@
 <?
 
 	include_once 'common.php';
-	
-	if ($sizefile<$def['booru']['filesize']) {
+
+	if ($sizefile < def::art('filesize')) {
 		if (is_array($check)) {
-			$md5=md5_file($temp);
+			$md5 = md5_file($temp);
 			if (
-				!obj::db()->sql('select id from art where md5="'.$md5.'"',2,'id') && 
-				!obj::db('sub')->sql('select id from w8m_art where md5="'.$md5.'"',2) && 
-				!obj::db('sub')->sql('select id from art where variation like "%.'.$md5.'.%"',2)
+				!Database::get_count('art', 'md5 = ?', $md5) &&
+				!Database::get_count('art_variation', 'md5 = ?', $md5)
 			) {
-				$extension = strtolower(pathinfo($file,PATHINFO_EXTENSION));
+
+				$extension = strtolower(pathinfo($file, PATHINFO_EXTENSION));
 				$thumb=md5(microtime(true));
 				$newname = $md5.'.'.$extension;
-				$newfile = ROOT_DIR.SL.'images'.SL.'booru'.SL.'full'.SL.$newname;
-				$newthumb = ROOT_DIR.SL.'images'.SL.'booru'.SL.'thumbs'.SL.$thumb.'.jpg';
-				$newlargethumb = ROOT_DIR.SL.'images'.SL.'booru'.SL.'thumbs'.SL.'large_'.$thumb.'.jpg';
+				$newfile = IMAGES.SL.'booru'.SL.'full'.SL.$newname;
+				$newresized = IMAGES.SL.'booru'.SL.'resized'.SL.$md5.'.jpg';
+				$newthumb = IMAGES.SL.'booru'.SL.'thumbs'.SL.$thumb.'.jpg';
+				$newlargethumb = IMAGES.SL.'booru'.SL.'thumbs'.SL.'large_'.$thumb.'.jpg';
 				chmod($temp, 0755);
-				if (!move_uploaded_file($temp, $newfile)) file_put_contents($newfile, file_get_contents($temp));
-				$a = microtime(true);
-				$imagick =  new $image_class($path = $newfile);
-				$sizes = $imagick->getImageWidth().'x'.$imagick->getImageHeight();
-				if ($imagick->getImageWidth() > $def['booru']['resizewidth']*$def['booru']['resizestep']) {
-					if (scale($def['booru']['resizewidth'],ROOT_DIR.SL.'images/booru/resized/'.$md5.'.jpg',95,false))
-						$resized = $sizes;
-				} elseif ($sizefile > $def['booru']['resizeweight']) {
-					if (scale(false,ROOT_DIR.SL.'images/booru/resized/'.$md5.'.jpg',95,false))
-						$resized = $sizes;
+
+				if (!move_uploaded_file($temp, $newfile)) {
+					file_put_contents($newfile, file_get_contents($temp));
 				}
-						
-				if (!empty($resized)) {					
+
+				$imagick =  new $image_class($path = $newfile);
+				$animated = 0;
+
+				$sizes = $imagick->getImageWidth().'x'.$imagick->getImageHeight();
+				if ($imagick->getImageWidth() > def::art('resizewidth')*def::art('resizestep')) {
+					if (scale(def::art('resizewidth'), $newresized, 95, false)) {
+						$resized = $sizes;
+					}
+				} elseif ($sizefile > def::art('resizeweight')) {
+					if (scale(false, $newresized, 95, false)) {
+						$resized = $sizes;
+					}
+				}
+
+				if (!empty($resized)) {
 					if ($sizefile > 1024*1024) {
 						$sizefile = round($sizefile/(1024*1024),1).' мб';
 					} elseif ($sizefile > 1024) {
@@ -39,23 +47,24 @@
 					}
 					$resized .= 'px; '.$sizefile;
 				}
-				
-				$animated = 0;
-				
-				scale($def['booru']['largethumbsize'],$newlargethumb);
-				scale($def['booru']['thumbsize'],$newthumb);
-				
+
+				scale(def::art('largethumbsize'), $newlargethumb);
+				scale(def::art('thumbsize'), $newthumb);
+
 				$result = array(
-					'success' => true, 
-					'image' => SITE_DIR.'/images/booru/thumbs/'.$thumb.'.jpg', 
-					'md5' => $md5, 
-					'data' => $md5.'#'.$thumb.'#'.$extension.'#'.$resized.'#'.$animated, 
+					'success' => true,
+					'image' => '/images/booru/thumbs/'.$thumb.'.jpg',
+					'md5' => $md5,
+					'data' => $md5.'#'.$thumb.'#'.$extension.'#'.$resized.'#'.$animated,
 				);
+			} else {
+				$result = array('error' => 'already-have');
 			}
-			else {$result = array('error' => 'already-have');}
+		} else {
+			$result = array('error' => 'filetype');
 		}
-		else {$result = array('error' => 'filetype');}
+	} else {
+		$result = array('error' => 'maxsize');
 	}
-	else {$result = array('error' => 'maxsize');} 
 
 echo htmlspecialchars(json_encode($result), ENT_NOQUOTES);
--- engine/upload/imagick_substitute.php
+++ engine/upload/imagick_substitute.php
@@ -17,10 +17,10 @@ class imagick_substitute {
 	private $height;
 	private $format;
 	private $compression = 80;
-	
+
 	const COMPOSITE_OVER = 'over';
 	const COMPRESSION_JPEG = 'jpeg';
-	
+
 	function __construct ($file) {
 		$this->path = $file;
 		$sizes = getimagesize($this->path);
@@ -33,104 +33,104 @@ class imagick_substitute {
 			case 'jpeg': $this->data = imagecreatefromjpeg($this->path); break;
 			case 'gif': $this->data = imagecreatefromgif($this->path); break;
 			default: die;
-		}		
+		}
 	}
-	
+
 	function __call($name, $arguments) {
 		if($name == 'clone') return $this;
 		else return false;
 	}
-	
+
 	function getImageWidth() {
 		return $this->width;
 	}
-	
+
 	function getImageHeight() {
 		return $this->height;
 	}
-	
-	function getImageFormat() {		
+
+	function getImageFormat() {
 		return $this->format;
 	}
-	
-	function getFilename() {		
+
+	function getFilename() {
 		return $this->path;
 	}
-	
-	function hasNextImage() {		
+
+	function hasNextImage() {
 		return false;
 	}
-	
+
 	function setImageCompressionQuality($compression) {
 		$this->compression = $compression;
 	}
-	
-	function thumbnailImage($x, $y, $bestfit=false) 
+
+	function thumbnailImage($x, $y, $bestfit=false)
 	{
 		if($bestfit)
 		{
 			if($this->height > $this->width) $x = $this->width/($this->height/$y);
 			else $y = $this->height/($this->width/$x);
 		}
-		
+
 		$thumbnail = imagecreatetruecolor($x, $y);
 		imagecopyresampled($thumbnail,$this->data,0,0,0,0,$x,$y,$this->width,$this->height);
 		$this->data = $thumbnail;
 	}
-	
-	function scaleImage($x, $y, $bestfit=false) 
+
+	function scaleImage($x, $y, $bestfit=false)
 	{
 		$this->thumbnailImage($x, $y, $bestfit);
 	}
-	
+
 	function colorFloodFillImage($fill, $fuzz, $bordercolor, $x, $y) {
 		list($r, $g, $b) = str_split(substr($fill, 1),2);
 		$color = imagecolorallocate(
-					$this->data, 
-					'0x' . strtoupper($r), 
-					'0x' . strtoupper($g), 
+					$this->data,
+					'0x' . strtoupper($r),
+					'0x' . strtoupper($g),
 					'0x' . strtoupper($b)
 				);
 		imagefill($this->data, $x, $y, $color);
 	}
-	
+
 	function compositeImage($image, $type, $x, $y) {
 		switch ($type) {
 			case 'over':
 				imagecopymerge(
-					$this->data, 
-					$image->data, 
-					$this->width, 
-					$this->height, 
-					$x, 
-					$y, 
-					$image->width, 
-					$image->height, 
+					$this->data,
+					$image->data,
+					$this->width,
+					$this->height,
+					$x,
+					$y,
+					$image->width,
+					$image->height,
 					100
 				);
 				break;
 			default: echo 'error-unexpected'; die;
 		}
 	}
-	
+
 	function setImageCompression($type) {
 		$this->format = $type;
 	}
-	
+
 	function setImageFormat($type) {
-		$this->format = $type;		
+		$this->format = $type;
 	}
-	
+
 	function writeImage($path) {
 		$function = 'image'.$this->format;
 		$compression = $this->format == 'png' ? floor(($this->compression - 1) / 10) : $this->compression;
 		$function($this->data, $path, $compression);
 	}
-	
+
 	function coalesceImages() {
-		return array($this);
-	}	
-	
+		return $this;
+	}
+
 	function clear() {
 		$vars = get_object_vars($this);
 		foreach ($vars as $key => $var) {
