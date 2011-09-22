0fc05a263ff380790a5e20bb83226e52c42c2247
4otaku <noname-nya@yandex.ru>
2011-09-22 22:37:56 +0400

    fix

--- libs/dynamic/search.php
+++ libs/dynamic/search.php
@@ -1,4 +1,4 @@
-<? 
+<?
 
 class dynamic__search
 {
@@ -7,33 +7,33 @@ class dynamic__search
 	function searchtip() {
 		 global $check; global $search;
 		if (!$search) $search = new search();
-		$query = mysql_real_escape_string($search->prepare_string(urldecode(query::$get['data']),true));
+		$query = $search->prepare_string(urldecode(query::$get['data']),true);
 		if (!empty($query)) {
-			
+
 			if (!in_array(query::$get['area'],$this->areas)) {
 				$area = str_split(query::$get['area']);
 				foreach ($area as &$one) $one = $this->areas[$one];
 				$area = array_filter($area);
 				unset($one);
 			}
-			else $area = array(query::$get['area']);	
-			
+			else $area = array(query::$get['area']);
+
 			$order = implode('+',$area);
 			$where = 'and ('.implode('>0 or ',$area).'> 0)';
-					
+
 			$variants[] = obj::db()->sql('select id, query, query as alias, "search" as type from search_queries where (Left(query , '.mb_strlen($query).') = "'.$query.'" '.$where.') order by '.$order.' desc limit 10','id');
-			
+
 			if (query::$get['area'] == 'a' || query::$get['area'] == 'p' || query::$get['area'] == 'v') {
 				switch (query::$get['area']) {
 					case 'a': $area = "art"; break;
 					case 'p': $area = "post"; break;
 					case 'v': $area = "video"; break;
 					default: break;
-				}				
+				}
 				$variants[] = obj::db()->sql('select id, alias, name as query, "tag" as type from tag where ((Left(alias , '.mb_strlen($query).') = "'.$query.'" or Left(name, '.mb_strlen($query).') = "'.$query.'" or locate("|'.$query.'",tag.variants)) and '.$area.'_main > 0) order by '.$area.'_main desc limit 2','id');
 				$variants[] = obj::db()->sql('select id, alias, name as query, "category" as type from category where ((Left(alias , '.mb_strlen($query).') = "'.$query.'" or Left(name, '.mb_strlen($query).') = "'.$query.'") and locate("|'.$area.'|",category.area)) limit 2','id');
 				$variants[] = obj::db()->sql('select id, alias, name as query, "language" as type from language where (Left(alias , '.mb_strlen($query).') = "'.$query.'" or Left(name, '.mb_strlen($query).') = "'.$query.'") limit 2','id');
-//				$variants[] = obj::db()->sql('select id, alias, name as query, "author" as type from author where (Left(alias , '.mb_strlen($query).') = "'.$query.'" or Left(name, '.mb_strlen($query).') = "'.$query.'") limit 2','id');			
+//				$variants[] = obj::db()->sql('select id, alias, name as query, "author" as type from author where (Left(alias , '.mb_strlen($query).') = "'.$query.'" or Left(name, '.mb_strlen($query).') = "'.$query.'") limit 2','id');
 			}
 
 			$return = array();
