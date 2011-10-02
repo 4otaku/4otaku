8787b1e3b94935a6a7c1021e56a8626cacf58a13
4otaku <noname-nya@yandex.ru>
2011-10-02 16:40:12 +0400

    Немного битья хрусталя

--- ajax.php
+++ ajax.php
@@ -15,13 +15,21 @@ $func = query::$get['f'];
 
 $data = $output->$func();
 if (isset($output->template) || $data) {
-    if (!empty($output->textarea)) ob_end_clean();
-    if (isset($output->template)) include_once($output->template);
-    else include_once TEMPLATE_DIR.SL.'dinamic'.SL.query::$get['m'].SL.query::$get['f'].'.php';
+	if (!empty($output->textarea)) {
+		ob_end_clean();
+	}
+
+	$url = query::$url;
+
+	if (isset($output->template)) {
+		include_once($output->template);
+	} else {
+		include_once TEMPLATE_DIR.SL.'dinamic'.SL.query::$get['m'].SL.query::$get['f'].'.php';
+	}
 }
 
-if (isset($postparse)) {
-	preg_match($postparse, ob_get_clean(), $buffer);
+if (!empty($output->postparse)) {
+	preg_match($output->postparse, ob_get_clean(), $buffer);
 	echo myoutput(end($buffer));
 } else {
 	ob_end_flush();
