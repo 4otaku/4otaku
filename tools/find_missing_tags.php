fd04b038dc990200b9f204a7919234b2af9eee2c
Nameless <admin@4otaku.ru>
2011-11-04 22:38:39 +0400

    tool for tags inconsistence

---
+++ tools/find_missing_tags.php
@@ -0,0 +1,36 @@
+<?php
+
+include '../inc.common.php';
+
+$arts = Database::get_vector('art', array('id', 'tag'));
+
+$all_tags = Database::get_vector('tag', array('alias', 'id'));
+
+foreach ($arts as $id => $tags) {	
+	$tags = array_unique(array_filter(explode('|', $tags)));
+	
+	foreach ($tags as $tag) {
+		if (empty($all_tags[$tag])) {
+			@$all_tags[$tag][] = $id;
+		}
+	}
+}
+
+$arts = array();
+
+foreach ($all_tags as $tag => $data) {
+	if (!is_array($data)) {
+		unset($all_tags[$tag]);
+	} else {
+		foreach ($data as $art) {
+			@$arts[$art][] = $tag;
+		}
+	}
+}
+
+foreach ($arts as $art => $tags) {
+	?>
+		<a href="/art/<?=$art;?>">Арт №<?=$art;?></a>, теги
+		 <?=implode(', ', $tags);?> <br />
+	<?php
+}
