87ca330078adea12ec7937dddc32fa3e2018f02e
Nameless <admin@4otaku.ru>
2011-11-05 13:17:09 +0400

    Добавил ссылки на справку по добавлению

--- templates/dinamic/add/post.php
+++ templates/dinamic/add/post.php
@@ -42,6 +42,12 @@
 					<input type="submit" class="disabled add_link" rel="main" value="+" />
 				</td>
 			</tr>
+			<tr>
+				<td colspan="2">
+					<a href="http://wiki.4otaku.ru/%D0%94%D0%BE%D0%B1%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D0%B8" target="_blank">
+						Справка по добавлению записи</a>.
+				</td>
+			</tr>
 		</tfoot>
     </table>
 	<div class="fields">
--- templates/dinamic/add/video.php
+++ templates/dinamic/add/video.php
@@ -25,6 +25,12 @@
 					</span>
 				</td>
 			</tr>
+			<tr>
+				<td colspan="2">
+					<a href="http://wiki.4otaku.ru/%D0%94%D0%BE%D0%B1%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B2%D0%B8%D0%B4%D0%B5%D0%BE" target="_blank">
+						Справка по добавлению видео</a>.
+				</td>
+			</tr>
         </tbody>
     </table>
 	<div class="fields">
