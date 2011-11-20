2863be78c84344f45951a778cd5f4cc8c70c02ca
Nameless <admin@4otaku.ru>
2011-11-20 15:33:06 +0400

    навигация по микседу

---
+++ libs/query/meta.php
@@ -0,0 +1,81 @@
+<?php
+
+class Query_Meta
+{
+	protected $mode_add = true;
+	protected $meta = array();
+	protected $type = false;
+	
+	protected static $meta_plural_singular = array(
+		'categories' => 'category',
+		'tags' => 'tag',
+		'languages' => 'language',
+		'authors' => 'author'
+	);
+	
+	public function __construct($data, $type, $sign = '+') {
+		
+		if ($sign == '-') {
+			$this->mode_add = false;
+		}
+		
+		$this->meta = (array) $data;
+		
+		if (array_key_exists($type, self::$meta_plural_singular)) {
+			$this->type = self::$meta_plural_singular[$type];
+		}
+		
+		if (in_array($type, self::$meta_plural_singular)) {
+			$this->type = $type;
+		}		
+	}
+	
+	public function get_condition() {
+		if (empty($this->type)) {
+			return '';
+		}
+		
+		$return = ' and (';	
+		
+		$data = array();
+		for ($i = 0; $i < count($this->meta); $i++) {
+			$data[] = ($this->mode_add ? '' : '!') . 
+				'locate(?, '.$this->type.')';
+		}
+		
+		$return .= implode(' or ', $data);
+		
+		$return .= ')';
+		
+		return $return;
+	}
+	
+	public function get_params() {
+		if (empty($this->type)) {
+			return array();
+		}
+		
+		$data = $this->meta;
+		foreach ($data as &$item) {
+			$item = '|' . $item . '|';
+		}
+		
+		return $data;
+	}
+	
+	public function is_simple() {
+		return $this->type && $this->mode_add && (count($this->meta) == 1);
+	}
+	
+	public function get_type() {
+		return $this->type;
+	}
+	
+	public function get_sign() {
+		return $this->mode_add ? '+' : '-';
+	}
+	
+	public function get_meta() {
+		return implode(',', $this->meta);
+	}
+}
