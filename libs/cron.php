ec714e28f823d35c82c534e364427e4bdbedf6df
Nameless <admin@4otaku.ru>
2011-12-10 22:44:17 +0400

    cleanup

--- libs/cron.php
+++ libs/cron.php
@@ -38,53 +38,7 @@ class Cron
 			}
 		}
 	}
-
-	function gouf_check_single($base, $link){
-		$link = str_replace('&apos;',"'",html_entity_decode($link,ENT_QUOTES,'UTF-8'));
-		$return = 'works';
-		foreach ($base as $one) if (stristr($link,$one['alias'])) {
-
-			$input = $this->gouf_curl(str_replace(' ','%20',stripslashes($link)), ($one['alias'] == 'mediafire.com'));
-
-			if (!trim($input) || ($one['alias'] == 'megaupload.com' && stristr($input,'http://www.megaupload.com/?c=msg')))
-				return 'unknown';
-
-			$return = 'error';
-			if ($input) {
-				$tests = explode('|',$one['text']);
-				foreach ($tests as $test) if (stristr($input,$test)) $return = 'works';
-			}
-			break;
-
-		}
-
-		if ($return == 'error' && $one['alias'] == '4shared.com') {
-			$fh = fopen("test.txt", 'w');
-			fwrite($fh, $link."\n\n".implode("/n", $tests)."\n\n".$input);
-			fclose($fh);
-		}
-		return $return;
-	}
-
-	function gouf_curl($link, $simple=false) {
-
-		if (!$simple) {
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-			curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)');
-			curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-			curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
-			curl_setopt($ch, CURLOPT_URL, $link);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			$input = curl_exec($ch);
-			curl_close($ch);
-		}
-		else $input = @file_get_contents($link);
-		return $input;
-	}
-
-	/* Gouf закончился */
-
+	
 	function clean_tags() {
 		global $def;
 /*		foreach ($def['type'] as $type) $query .= ' union select concat("'.$type.'",id) as id, tag, area, "'.$type.'" as type from '.$type;
