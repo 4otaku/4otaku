b2771fc3eb72dd9f9b0cd18d99071c0a07e297d1
Nameless <admin@4otaku.ru>
2011-12-18 10:37:34 +0400

    Сортировка ссылок

--- jss/edit/links.js
+++ jss/edit/links.js
@@ -1,42 +1,15 @@
 $(document).ready(function(){
 
-	$(".arrow-up:first").hide();
-	$(".arrow-down:visible:last").hide();
-	
-	$(".arrow-up").live('click',function(){
-		window.LinkValues = new Array();
-		$(this).parents("tr.link").find("input, select").each(function(){
-			if ($(this).attr('name') != undefined && $(this).attr('name').split('][').length > 1) {
-				window.LinkValues[$(this).attr('name').split('][')[1]] = $(this).val();
-				window.LinkIndex = $(this).attr('name').split('][')[0].substr(5, $(this).attr('name').split('][')[0].length - 1);
-			}
-		});
-		$(this).parents("tr.link:visible").prev().find("input, select").each(function(){
-			if ($(this).attr('name') != undefined && $(this).attr('name').split('][').length > 1) {
-				ThisName = $(this).attr('name').split('][')[1].substr(0, $(this).attr('name').split('][')[1].length - 1);
-				if ($(this).is('input')) $("input[name$=link\\["+window.LinkIndex+"\\]\\["+ThisName+"\\]]").val($(this).val());
-				if ($(this).is('select')) $("select[name$=link\\["+window.LinkIndex+"\\]\\["+ThisName+"\\]]").val($(this).val());
-				$(this).val(window.LinkValues[$(this).attr('name').split('][')[1]]);
-			}
-		});
-	});
-	
-	$(".arrow-down").live('click',function(){
-		window.LinkValues = new Array();
-		$(this).parents("tr.link").find("input, select").each(function(){
-			if ($(this).attr('name') != undefined && $(this).attr('name').split('][').length > 1) {
-				window.LinkValues[$(this).attr('name').split('][')[1]] = $(this).val();
-				window.LinkIndex = $(this).attr('name').split('][')[0].substr(5, $(this).attr('name').split('][')[0].length - 1);
-			}
-		});
-		$(this).parents("tr.link:visible").next().find("input, select").each(function(){
-			if ($(this).attr('name') != undefined && $(this).attr('name').split('][').length > 1) {
-				ThisName = $(this).attr('name').split('][')[1].substr(0, $(this).attr('name').split('][')[1].length - 1);
-				if ($(this).is('input')) $("input[name$=link\\["+window.LinkIndex+"\\]\\["+ThisName+"\\]]").val($(this).val());
-				if ($(this).is('select')) $("select[name$=link\\["+window.LinkIndex+"\\]\\["+ThisName+"\\]]").val($(this).val());
-				$(this).val(window.LinkValues[$(this).attr('name').split('][')[1]]);
-			}
-		});
+	$('.sortable').sortable({
+		containment: '.sortable-holder',
+		handle: '.handler',
+		forceHelperSize: true,
+		forcePlaceholderSize: true,
+		start: function(event, ui) {
+			ui.item.find('.remove_link').css('visibility', 'hidden');
+		},
+		stop: function(event, ui) {
+			ui.item.find('.remove_link').css('visibility', 'visible');
+		}	
 	});	
-	
 });  
--- templates/dinamic/edit/art_translations.php
+++ templates/dinamic/edit/art_translations.php
@@ -1,7 +1,7 @@
 <? 
 include_once('templates'.SL.'dinamic'.SL.'edit'.SL.'top.php');
 ?>
-<script type="text/javascript" src="<?=$def['site']['dir']?>/jss/m/?b=jss&f=edit_form.js,edit/ui.js,edit/translations.js"></script>
+<script type="text/javascript" src="<?=$def['site']['dir']?>/jss/m/?b=jss&f=ui/core.js,ui/draggable.js,ui/resizeable.js,edit_form.js,edit/translations.js"></script>
 <br /><span class="add_translation">Чтобы добавить перевод, кликните на свободное место.</span> 
 Автор перевода: <input type="text" name="author" class="translation_author margin20" value="<?=$sets['user']['name'];?>" /><br />
 <? 
