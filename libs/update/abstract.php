8a734eb926e4df0d9e8d25d8cc71c74ccf0ff524
Nameless <admin@4otaku.ru>
2011-12-24 22:16:56 +0400

    fixes

--- libs/update/abstract.php
+++ libs/update/abstract.php
@@ -3,26 +3,36 @@
 abstract class Update_Abstract extends Abstract_Action
 {
 	public function main() {
-		
+
 		Database::begin();
-		
+
 		try {
-			
+
 			foreach (query::$post as $key => $value) {
 
 				if (method_exists($this, $key)) {
 					$this->$key(query::$post);
 				}
 			}
+
+			if (
+				!empty(query::$post['edit']) &&
+				!array_key_exists(query::$post['edit'], query::$post) &&
+				method_exists($this, query::$post['edit'])
+			) {
+				$function = query::$post['edit'];
+				$this->$function(query::$post);
+			}
+
 			$this->save_changes();
-			
+
 			Database::commit();
 		} catch (PDOException $e) {
-			
+
 			Database::rollback();
 		}
-		
+
 	}
-	
+
 	abstract protected function save_changes();
 }
