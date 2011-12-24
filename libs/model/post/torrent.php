5ba551bb53028c32f6dbd6cf1efc56e4c2618056
Nameless <admin@4otaku.ru>
2011-12-24 22:51:54 +0400

    fixes

--- libs/model/post/torrent.php
+++ libs/model/post/torrent.php
@@ -3,7 +3,7 @@
 class Model_Post_Torrent extends Model_Abstract
 {
 	const MAX_DISPLAY_FILENAME = 40;
-	
+
 	// Поля таблицы
 	protected $fields = array(
 		'id',
@@ -24,7 +24,7 @@ class Model_Post_Torrent extends Model_Abstract
 	public function __construct($data = array()) {
 
 		parent::__construct($data);
-		
+
 		$this->set('display_size', round($this->get('size'), 2));
 		$this->set('display_sizetype', $this->sizetypes[$this->get('sizetype')]);
 		$this->set('display_file', $this->get_display_filename());
@@ -35,16 +35,16 @@ class Model_Post_Torrent extends Model_Abstract
 		$this->get_torrent_size();
 
 		parent::insert();
-		
+
 		Database::db('tracker')->insert('xbt_files', array(
-			'info_hash' => $this->get('hash'),
+			'info_hash' => utf8_decode($this->get('hash')),
 			'mtime' => time(),
 			'ctime' => time(),
 		));
 
 		return $this;
 	}
-	
+
 	protected function get_torrent_size() {
 		$file = $this->get('file');
 		$hash = $this->get('hash');
@@ -55,7 +55,7 @@ class Model_Post_Torrent extends Model_Abstract
 
 		$path = FILES . SL . 'torrent' . SL . $hash . SL . $file;
 		$torrent = new Transform_Torrent($path);
-		
+
 		if ($torrent->get_hash() != $hash) {
 			throw new Error('Incorrect torrent hash');
 		}
@@ -77,12 +77,12 @@ class Model_Post_Torrent extends Model_Abstract
 		if (strlen($file) <= self::MAX_DISPLAY_FILENAME + 4) {
 			return $file;
 		}
-		
+
 		$part_length = ceil(self::MAX_DISPLAY_FILENAME / 2);
-		
+
 		$begin = substr($file, 0, $part_length);
 		$end = substr($file, -1 * $part_length);
-		
+
 		return $begin . '...' . $end;
 	}
 }
