ae57b73ec6bcb3b7eb4b0e765bc5e3362f76f82b
Nameless <admin@4otaku.ru>
2011-12-24 20:43:41 +0400

    Сообщение о том, что идут работы

---
+++ maintenance.php
@@ -0,0 +1,9 @@
+<div style="text-align: center; margin-top: 40px;">
+	<img src="/images/maintenance.png" />
+	<br /><br />
+	Ориентировочное время возобновления работы 24 декабря (то есть сегодня), в 25:30.
+	<br />
+	Если есть вопросы, то к вашим услугам конференция сайта: main@room.4otaku.ru.
+	<a href="http://jabberworld.info/%D0%9A%D0%BB%D0%B8%D0%B5%D0%BD%D1%82%D1%8B_Jabber">
+		Помощь по настройке</a>.
+</div>
