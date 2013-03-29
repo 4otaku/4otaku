0b6e1bb1f92d01392389c26e588f48fba748282c
Nameless <admin@4otaku.ru>
2013-03-30 00:38:11 +0400

    fixes

--- libs/output/index.php
+++ libs/output/index.php
@@ -79,12 +79,15 @@ class output__index extends engine
 			$return['news']['sortdate'] = 0;
 		}
 
-		$return['links'] = (int) Database::join('post_link_url', 'pu.id = plu.url_id')
+		Database::join('post_link_url', 'pu.id = plu.url_id')
 			->join('post_link', 'pl.id = plu.link_id')
 			->join('post', 'p.id = pl.post_id')
-			->group('pu.id')
-			->get_count('post_url', 'pu.status = ? and p.area is not null and p.area != ?',
+			->group('pu.id')->limit(1)
+			->get_table('post_url', 'pu.id',
+				'pu.status = ? and p.area is not null and p.area != ?',
 				array(Cron_Post_Gouf::STATUS_BROKEN, 'deleted'));
+		$count = Database::sql('SELECT FOUND_ROWS();');
+		$return['links'] = (int) reset(reset($count));
 
 		return $return;
 	}
