f044f443335851c483e8c6b43f4e71a07f28b484
Nameless <admin@4otaku.ru>
2013-04-30 20:20:50 +0400

    logs

---
+++ libs/output/logs_kikaki.php
@@ -0,0 +1,10 @@
+<?php
+
+class output__logs_kikaki extends output__logs_abstract
+{
+	public static $room_ids = array(14);
+	protected $start_year = 2010;
+	protected $start_month = 12;
+	protected $start_day = 9;
+	public static $cache_key = 3;
+}
