56118684677222aa30c173596526f18610c6e387
Nameless <admin@4otaku.ru>
2013-05-01 14:28:16 +0400

    logs

---
+++ libs/output/logs_touhouplay.php
@@ -0,0 +1,10 @@
+<?php
+
+class output__logs_touhouplay extends output__logs_abstract
+{
+	public static $room_ids = array(34);
+	protected $start_year = 2012;
+	protected $start_month = 1;
+	protected $start_day = 6;
+	public static $cache_key = 4;
+}
