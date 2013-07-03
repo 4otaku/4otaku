e3c32d9cdd7e90b1da8df9f79f8885dcdf722723
shtrih <s@htgi.ru>
2013-07-04 08:15:00 +0900

    Обновил плашку с петицией, отключил таймер на кнопке, добавил неминифицированную версию скрипта плашки

--- templates/side/head.php
+++ templates/side/head.php
@@ -5,12 +5,11 @@
 	<meta http-equiv="Pragma" content="no-cache" />
 	<meta http-equiv="Expires" content="-1" />
 	<title><?=$data['head']['title'];?></title>
+	<link rel="stylesheet" href="<?=$def['site']['dir']?>/jss/m/?b=jss&f=plugins.css,main.css,header.css<?=(sets::user('rights') ? ',admin.css' : '');?>&ver=23" type="text/css" media="screen" />
 	<script type="text/javascript" src="<?=$def['site']['dir']?>/jss/config.js"></script>
-	<script type="text/javascript" src="<?=$def['site']['dir']?>/jss/m/?b=jss&f=jquery-1.6.2.min.js,plugins.js,main.js
+	<script type="text/javascript" src="<?=$def['site']['dir']?>/jss/m/?b=jss&f=lock.js,jquery-1.6.2.min.js,plugins.js,main.js
 		<?=(sets::user('rights') ? ',admin.js' : '');?>
 		<?=(sets::plugins(1) ? ',plugin/censor.js' : '');?>
 		<?=(sets::plugins(2) ? ',plugin/hider.js' : '');?>
 		&ver=36"></script>
-	<link rel="stylesheet" href="<?=$def['site']['dir']?>/jss/m/?b=jss&f=plugins.css,main.css,header.css<?=(sets::user('rights') ? ',admin.css' : '');?>&ver=23" type="text/css" media="screen" />
-	<!--<script type="text/javascript" src="<?=$def['site']['dir']?>/jss/lock.min.js"></script>-->
 </head>
