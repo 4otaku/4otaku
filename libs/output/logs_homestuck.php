f5e64d05b8256298c797f7d0af89625951b874d5
root <root@Ubuntu-1204-precise-64-minimal.local>
2013-09-07 13:20:43 +0200

    homestuck_logs

---
+++ libs/output/logs_homestuck.php
@@ -0,0 +1,10 @@
+<?php
+
+class output__logs_homestuck extends output__logs_abstract
+{
+	public static $room_ids = array(39);
+	protected $start_year = 2012;
+	protected $start_month = 6;
+	protected $start_day = 18;
+	public static $cache_key = 5;
+}
