024bb4a1e20970886b40ba44b955d3cdee827bbb
Konstantin Pakshaev <derinov@odesk.com>
2013-11-09 00:26:01 +0400

    add social plugin

--- jss/plugin/social.js
+++ jss/plugin/social.js
@@ -9,34 +9,36 @@
 // @copyright  2013+, shtrih
 // ==/UserScript==
 
-function scriptBody() {
-	var icon = 'https://vk.com/images/vk16.png',
-		url = 'http://vk.com/share.php?url=',
-		post_header = $('.display_item h2'),
-		vk_button = $('<a/>', {
-			text: 'Поделиться',
-			target: '_blank',
-			css: {
-				padding:    '0 0 0 20px',
-				margin:     '0 0 0 10px',
-				background: 'url(' + icon + ') no-repeat 2px center',
-				'font-size':  '0.7em'
-			}
-		})
-		;
-
-	post_header.each(function () {
-		var post_link = $('a', this).attr('href'),
-			share_url = url + 'http://4otaku.org' + encodeURI(post_link)
+$(function(){
+	function scriptBody() {
+		var icon = 'https://vk.com/images/vk16.png',
+			url = 'http://vk.com/share.php?url=',
+			post_header = $('.display_item h2'),
+			vk_button = $('<a/>', {
+				text: 'Поделиться',
+				target: '_blank',
+				css: {
+					padding:    '0 0 0 20px',
+					margin:     '0 0 0 10px',
+					background: 'url(' + icon + ') no-repeat 2px center',
+					'font-size':  '0.7em'
+				}
+			})
 			;
 
-		$(this).append(
-			vk_button.clone().attr('href', share_url)
-		);
-	});
-}
+		post_header.each(function () {
+			var post_link = $('a', this).attr('href'),
+				share_url = url + 'http://4otaku.org' + encodeURI(post_link)
+				;
 
-var script = document.createElement('script');
-script.textContent = '(' + scriptBody.toString() + ')()';
-(document.head||document.documentElement).appendChild(script);
-script.parentNode.removeChild(script);
+			$(this).append(
+				vk_button.clone().attr('href', share_url)
+			);
+		});
+	}
+
+	var script = document.createElement('script');
+	script.textContent = '(' + scriptBody.toString() + ')()';
+	(document.head||document.documentElement).appendChild(script);
+	script.parentNode.removeChild(script);
+});
