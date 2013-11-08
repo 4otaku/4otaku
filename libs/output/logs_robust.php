74a955a68e6a06b17077efc22ed8f6400bdd2feb
root <root@Ubuntu-1204-precise-64-minimal.local>
2013-11-08 19:47:28 +0100

    add new rooms

---
+++ libs/output/logs_robust.php
@@ -0,0 +1,10 @@
+<?php
+
+class output__logs_robust extends output__logs_abstract
+{
+	public static $room_ids = array(49);
+	protected $start_year = 2013;
+	protected $start_month = 7;
+	protected $start_day = 3;
+	public static $cache_key = 8;
+}
---
+++ libs/output/logs_vn.php
@@ -0,0 +1,10 @@
+<?php
+
+class output__logs_vn extends output__logs_abstract
+{
+	public static $room_ids = array(54);
+	protected $start_year = 2013;
+	protected $start_month = 10;
+	protected $start_day = 30;
+	public static $cache_key = 9;
+}
